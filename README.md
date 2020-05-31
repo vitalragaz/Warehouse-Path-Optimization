58c2de0ec68c282f0db409e95783018a7975068e
Kai Krause <kkrause95@gmail.com>
2020-05-31 18:11:49 +0200

    updated readme, gitiggnore

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Lager-Wegeoptimierung
+# A Multi Aisle Warehouse with Many Pickers Traveling Salesmen vs. Ant Colony Optimization 
+
+## Description
+Abstract goes here
+
+## Setup
+Prerequisite:
+* npm
+
+The project can be run by doing the following steps:
+
+* Clone the repository localy
+* Install the required packages with `npm i`
+* Spin up the wheel with `npm run babel-transpile` 
+* Open the `app/index.html` in a ES6 compatible browser
+
+# Credits
+This repository is based on the work from Flur3x:
+https://github.com/Flur3x/Warehouse-Path-Optimization
--- app/js/grid.js
+++ app/js/grid.js
@@ -1,14 +1,9 @@
-var grid = new Grid(8, 24, 40),
-    testJobs = {
-      1: [19, 38 , 5],
-      2: [130, 60, 40],
-      3: [94, 49, 146]
-    };
+var grid = new Grid(8, 24, 40);
 
-function Grid(lanes, slotsInLane, slotSize) {
+function Grid(lanes, slotsInLane, slotPixelSize) {
   this.lanes = lanes;
   this.slotsInLane = slotsInLane;
-  this.slotPixelSize = slotSize;
+  this.slotPixelSize = slotPixelSize;
 }
 
 Grid.prototype.getLanes = function() {
@@ -24,7 +19,7 @@ Grid.prototype.getSlotPixelSize = function() {
 };
 
 Grid.prototype.getSlotNr = function(lane, slotPositionInLane) {
-  return ((lane  - 1) * this.slotsInLane) + slotPositionInLane;
+  return (lane - 1) * this.slotsInLane + slotPositionInLane;
 };
 
 Grid.prototype.getWaypointNr = function(slotNr) {
@@ -32,7 +27,6 @@ Grid.prototype.getWaypointNr = function(slotNr) {
 };
 
 Grid.prototype.render = function() {
-
   renderGrid();
 
   function renderGrid() {
@@ -40,75 +34,82 @@ Grid.prototype.render = function() {
 
     for (var lane = 1; lane <= grid.getLanes(); lane++) {
       for (var slot = 1; slot <= grid.getSlotsInLane(); slot++) {
-
         var slotNr = grid.getSlotNr(lane, slot);
         var waypointNr = grid.getWaypointNr(slotNr);
 
         if (slot % 2 != 0) {
-
           // Waypoint
-          $('<div id=' + "waypoint-" + waypointNr + '/>').css({
-            'position': 'absolute',
-            'top': (slot / 2 * size) + slot * 2,
-            'left': (3 * lane * size) + (5 * lane) - (2 * size),
-            'width': size,
-            'height': size,
-            'z-index': -1
-          }).appendTo('.grid-container');
+          $("<div id=" + "waypoint-" + waypointNr + "/>")
+            .css({
+              position: "absolute",
+              top: (slot / 2) * size + slot * 2,
+              left: 3 * lane * size + 5 * lane - 2 * size,
+              width: size,
+              height: size,
+              "z-index": -1
+            })
+            .appendTo(".grid-container");
 
           // Left Slot
-          $('<div id=' + "slot-" + slotNr + '><span class="slot-nr">'+slotNr+'</span></div>').css({
-            'position': 'absolute',
-            'top': 0,
-            'right': size,
-            'width': size,
-            'height': size,
-            'color': '#00BFFF',
-            'border': '1px',
-            'border-style': 'solid',
-            'z-index': -1
-          }).appendTo('#waypoint-' + waypointNr);
+          $("<div id=" + "slot-" + slotNr + '><span class="slot-nr">' + slotNr + "</span></div>")
+            .css({
+              position: "absolute",
+              top: 0,
+              right: size,
+              width: size,
+              height: size,
+              color: "#00BFFF",
+              border: "1px",
+              "border-style": "solid",
+              "z-index": -1
+            })
+            .appendTo("#waypoint-" + waypointNr);
         } else {
-
           // Right Slot
-          $('<div id=' + "slot-" + slotNr + '><span class="slot-nr">'+slotNr+'</span></div>').css({
-            'position': 'absolute',
-            'top': 0,
-            'left': size,
-            'width': size,
-            'height': size,
-            'color': '#00BFFF',
-            'border': '1px',
-            'border-style': 'solid',
-            'z-index': -1
-          }).appendTo('#waypoint-' + waypointNr);
+          $("<div id=" + "slot-" + slotNr + '><span class="slot-nr">' + slotNr + "</span></div>")
+            .css({
+              position: "absolute",
+              top: 0,
+              left: size,
+              width: size,
+              height: size,
+              color: "#00BFFF",
+              border: "1px",
+              "border-style": "solid",
+              "z-index": -1
+            })
+            .appendTo("#waypoint-" + waypointNr);
         }
 
         // Item
-        $('<div id=' + "item-" + slotNr + '>' + slotNr + '</div>').css({
-          'position': 'absolute',
-          'display': 'none',
-          'top': 2,
-          'left': 2,
-          'width': size - 6,
-          'height': size - 6,
-          'color': '#ffffff',
-          'text-align': 'center',
-          'line-height': size - 6 + 'px',
-          'background-color': '#3CB371',
-          'border': '1px',
-          'border-style': 'none',
-          'border-radius': 30
-        }).appendTo('#slot-' + slotNr);
+        $("<div id=" + "item-" + slotNr + ">" + slotNr + "</div>")
+          .css({
+            position: "absolute",
+            display: "none",
+            top: 2,
+            left: 2,
+            width: size - 6,
+            height: size - 6,
+            color: "#ffffff",
+            "text-align": "center",
+            "line-height": size - 6 + "px",
+            "background-color": "#3CB371",
+            border: "1px",
+            "border-style": "none",
+            "border-radius": 30
+          })
+          .appendTo("#slot-" + slotNr);
       }
     }
 
     // Table
-    $('<div id="optimizedJobTable"/>').css({
-      'position': 'absolute',
-      'top': (grid.getSlotsInLane()/2 * size) + (slot * 6),
-      'left': 0,
-      'z-index': -1
-    }).appendTo('.grid-container');
+    $('<div id="optimizedJobTable"/>')
+      .css({
+        position: "absolute",
+        top: (grid.getSlotsInLane() / 2) * size + slot * 6,
+        left: 0,
+        "z-index": -1
+      })
+      .appendTo(".grid-container");
   }
 };
